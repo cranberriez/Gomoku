13d036ad6d23ae412d1b3e04905f35b1a7c8fe00
CameronVoigt <283548@bsd48.org>
2018-05-21 10:31:54 -0700

    check win done

--- src/gomoku.java
+++ src/gomoku.java
@@ -14,6 +14,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+import com.sun.prism.paint.Color;
+
 public class gomoku implements ActionListener 
 {
 
@@ -121,49 +123,22 @@ public static void main(String[] args)
 						if (checkTie() == true)
 						{
 							clearBoard();
-							button[0][0].setEnabled(true);
-							button[0][1].setEnabled(true);
-							button[0][2].setEnabled(true);
-							button[1][0].setEnabled(true);
-							button[1][1].setEnabled(true);
-							button[1][2].setEnabled(true);
-							button[2][0].setEnabled(true);
-							button[2][1].setEnabled(true);
-							button[2][2].setEnabled(true);
 						}
 						else
 						{
-							if (checkWin(X_MOVE) == true)
+							if (checkWin(X_MOVE, j, i) == true)
 							{
 								//xwins yay
 								xwins++;
 								xname.setText(xPlayerName + " wins: " + xwins);
 								clearBoard();
-								button[0][0].setEnabled(true);
-								button[0][1].setEnabled(true);
-								button[0][2].setEnabled(true);
-								button[1][0].setEnabled(true);
-								button[1][1].setEnabled(true);
-								button[1][2].setEnabled(true);
-								button[2][0].setEnabled(true);
-								button[2][1].setEnabled(true);
-								button[2][2].setEnabled(true);
 							}
-							else if (checkWin(O_MOVE) == true)
+							else if (checkWin(O_MOVE, j, i) == true)
 							{
 								//o wins yay
 								owins++;
 								oname.setText(oPlayerName + " wins: " + owins);
 								clearBoard();
-								button[0][0].setEnabled(true);
-								button[0][1].setEnabled(true);
-								button[0][2].setEnabled(true);
-								button[1][0].setEnabled(true);
-								button[1][1].setEnabled(true);
-								button[1][2].setEnabled(true);
-								button[2][0].setEnabled(true);
-								button[2][1].setEnabled(true);
-								button[2][2].setEnabled(true);
 							}
 								
 						}
@@ -183,21 +158,234 @@ public static void main(String[] args)
 				xPlayerName = xChangeField.getText();
 				xname.setText(xPlayerName + " wins: " + xwins);
 			}
-			else if (event.getSource().equals(oChangeName) == true)
+			else if (event.getSource().equals(oChangeName) == true) //if you click on change name
 			{
 				oPlayerName = oChangeField.getText();
 				oname.setText(oPlayerName + " wins: " + owins);
 			}
-			else if (event.getSource().equals(reset))
+			else if (event.getSource().equals(reset)) //if you click reset
 			{
-				clearBoard();
+				clearBoard(); //clears the board and resets the buttons
 			}
 		}
 	}
 	
-	public boolean checkWin(int player) //goes through and checks every win combination
+	public boolean checkWin(int player, int x, int y) //goes through and checks every win combination
 	{
 		//look where the player placed the piece and check for a win
+		int total = 1;
+		System.out.println(x + " " + y);
+		if (player == X_MOVE)
+		{
+			/*
+			 * Check what's nearby
+			 * if there is a piece adjacent
+			 * follow the path and see if there is 5 in a row
+			 * check both directions and add
+			 */
+			//--------------------------------------------------------------------HOROZONTAL---------------------------------------
+			if (x > 0 && board[x-1][y] == player) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x > 1 && board[x-2][y] == player && x > 1) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x > 2 && board[x-3][y] == player) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x > 3 && board[x-4][y] == player && x > 3) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x < 14 && board[x+1][y] == player && x < 14) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x < 13 && board[x+2][y] == player) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x < 12 && board[x+3][y] == player) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x < 11 && board[x+4][y] == player) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (total >= 5)
+			{
+				return true;
+			}
+			
+			//----------------------------------------------------------------------VERTICAL----------------------------------------
+			total = 0;
+			if (y > 0 && board[x][y-1] == player) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (y > 1 && board[x][y-2] == player && x > 1) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (y > 2 && board[x][y-3] == player) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (y > 3 && board[x][y-4] == player && x > 3) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (y < 14 && board[x][y+1] == player && x < 14) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (y < 13 && board[x][y+2] == player) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (y < 12 && board[x][y+3] == player) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (y < 11 && board[x][y+4] == player) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (total >= 5)
+			{
+				return true;
+			}
+			
+			//-------------------------------------------------------------------DIAGNOL \--------------------------------------------
+			total = 1;
+			if (x > 0 && y > 0 && board[x-1][y-1] == player) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x > 1 && y > 1 && board[x-2][y-2] == player && x > 1) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x > 2 && y > 2 && board[x-3][y-3] == player) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x > 3 && y > 3 && board[x-4][y-4] == player && x > 3) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x < 14 && y < 14 && board[x+1][y+1] == player && x < 14) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x < 13 && y < 13 && board[x+2][y+2] == player) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x < 12 && y < 12 && board[x+3][y+3] == player) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x < 11 && y < 11 && board[x+4][y+4] == player) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (total >= 5)
+			{
+				return true;
+			}
+			//--------------------------------------------------------------DIAGNOL /--------------------------------------------------------
+			total = 1;
+			if (x < 14 && y > 0 && board[x+1][y-1] == player) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x < 13 && y > 1 && board[x+2][y-2] == player && x > 1) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x < 12 && y > 2 && board[x+3][y-3] == player) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x < 11 && y > 3 && board[x+4][y-4] == player && x > 3) //to the left of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x > 0 && y < 14 && board[x-1][y+1] == player && x < 14) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x > 1 && y < 13 && board[x-2][y+2] == player) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x > 2 && y < 12 && board[x-3][y+3] == player) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (x > 3 && y < 11 && board[x-4][y+4] == player) //to the right of the piece
+			{
+				total++;
+				System.out.println(total);
+			}
+			if (total >= 5)
+			{
+				return true;
+			}
+			
+			
+			
+		}
+		else if (player == O_MOVE)
+		{
+			return false;
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 		return false;
 	}
 	
@@ -226,7 +414,7 @@ public static void main(String[] args)
 			}
 			
 		}
-			turn = X_TURN;
+			turn = X_TURN; //its X's turn
 	}
 
 	
