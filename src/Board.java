6dca94f94c3c63e62b5df967dedabd0f15f7dc64
CameronVoigt <283548@bsd48.org>
2018-05-23 09:09:11 -0700

    Board

--- src/Board.java
+++ src/Board.java
@@ -9,10 +9,25 @@
 
 public class Board 
 {
+	//Variables and Objects
+		Image White Piece; //new image
+	
 	public Board()
-	{
+	{d
+		try
+		{
+			myImage = ImageIO.read(new File("asteroid.png")); //sets the new image to the asteroid picture
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
 		
 	}
-	
+	public void paintMe(Graphics g)
+	{
+		g.drawImage(myImage, x, y, null);
+	}
 	
 }
