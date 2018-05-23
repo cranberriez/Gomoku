073b8a81daa6f60988e3b3babc030f9edcfc226b
CameronVoigt <283548@bsd48.org>
2018-05-23 09:20:13 -0700

    broken pieces

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,9 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
 /*
  * 	class for the grid of the boar
  * 
@@ -10,24 +16,25 @@
 public class Board 
 {
 	//Variables and Objects
-		Image White Piece; //new image
+		Image White_Piece; //new image
+		int x;
+		int y;
 	
 	public Board()
-	{d
+	{	
 		try
 		{
-			myImage = ImageIO.read(new File("asteroid.png")); //sets the new image to the asteroid picture
+			White_Piece = ImageIO.read(new File("asteroid.JPEG")); //sets the new image to the asteroid picture
 		}
 		catch (Exception ex)
 		{
 			ex.printStackTrace();
 		}
 		
-		
 	}
 	public void paintMe(Graphics g)
 	{
-		g.drawImage(myImage, x, y, null);
+		g.drawImage(White_Piece, x, y, null);
 	}
 	
 }
