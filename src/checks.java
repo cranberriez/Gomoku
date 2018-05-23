9ee929f7104b66570710df635b59fa1793cffc62
cranberriez <jakevilevac@gmail.com>
2018-05-23 14:48:34 -0700

    Removed weird extras

--- src/checks.java
+++ src/checks.java
@@ -1,17 +1,17 @@
 /*
  *  Class for checking wins and ties
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  */
 
 
-public class checks 
+public class checks
 {
-	final int BLANK = 0; 
-	final int X_MOVE = 1; 
-	final int O_MOVE = 2; 
+	final int BLANK = 0;
+	final int X_MOVE = 1;
+	final int O_MOVE = 2;
 
 	public boolean checkWin(int[][] board, int player, int x, int y) //goes through and checks every win combination
 	{
@@ -25,25 +25,25 @@ public class checks
 		 */
 		//--------------------------------------------------------------------HOROZONTAL---------------------------------------
 		int total = 1;
-		
+
 		if (x > 0 && board[x-1][y] == player) //to the left of the piece
 		{
 			total++;
-			if (x > 1 && board[x-2][y] == player && x > 1) //to the left of the piece
+			if (x > 1 && board[x-2][y] == player) //to the left of the piece
 			{
 				total++;
 				if (x > 2 && board[x-3][y] == player) //to the left of the piece
 				{
 					total++;
-					if (x > 3 && board[x-4][y] == player && x > 3) //to the left of the piece
+					if (x > 3 && board[x-4][y] == player) //to the left of the piece
 					{
 						total++;
 					}
 				}
 			}
 		}
-		
-		if (x < 14 && board[x+1][y] == player && x < 14) //to the right of the piece
+
+		if (x < 14 && board[x+1][y] == player) //to the right of the piece
 		{
 			total++;
 			if (x < 13 && board[x+2][y] == player) //to the right of the piece
@@ -64,27 +64,27 @@ public class checks
 		{
 			return true;
 		}
-		
+
 		//----------------------------------------------------------------------VERTICAL----------------------------------------
 		total = 1;
 		if (y > 0 && board[x][y-1] == player) //to the left of the piece
 		{
 			total++;
-			if (y > 1 && board[x][y-2] == player && x > 1) //to the left of the piece
+			if (y > 1 && board[x][y-2] == player) //to the left of the piece
 			{
 				total++;
 				if (y > 2 && board[x][y-3] == player) //to the left of the piece
 				{
 					total++;
-					if (y > 3 && board[x][y-4] == player && x > 3) //to the left of the piece
+					if (y > 3 && board[x][y-4] == player) //to the left of the piece
 					{
 						total++;
 					}
 				}
 			}
 		}
-		
-		if (y < 14 && board[x][y+1] == player && x < 14) //to the right of the piece
+
+		if (y < 14 && board[x][y+1] == player) //to the right of the piece
 		{
 			total++;
 			if (y < 13 && board[x][y+2] == player) //to the right of the piece
@@ -100,32 +100,32 @@ public class checks
 				}
 			}
 		}
-		
+
 		if (total >= 5)
 		{
 			return true;
 		}
-		
+
 		//-------------------------------------------------------------------DIAGNOL \--------------------------------------------
 		total = 1;
 		if (x > 0 && y > 0 && board[x-1][y-1] == player) //to the left of the piece
 		{
 			total++;
-			if (x > 1 && y > 1 && board[x-2][y-2] == player && x > 1) //to the left of the piece
+			if (x > 1 && y > 1 && board[x-2][y-2] == player) //to the left of the piece
 			{
 				total++;
 				if (x > 2 && y > 2 && board[x-3][y-3] == player) //to the left of the piece
 				{
 					total++;
-					if (x > 3 && y > 3 && board[x-4][y-4] == player && x > 3) //to the left of the piece
+					if (x > 3 && y > 3 && board[x-4][y-4] == player) //to the left of the piece
 					{
 						total++;
 					}
 				}
 			}
 		}
-		
-		if (x < 14 && y < 14 && board[x+1][y+1] == player && x < 14) //to the right of the piece
+
+		if (x < 14 && y < 14 && board[x+1][y+1] == player) //to the right of the piece
 		{
 			total++;
 			if (x < 13 && y < 13 && board[x+2][y+2] == player) //to the right of the piece
@@ -141,7 +141,7 @@ public class checks
 				}
 			}
 		}
-		
+
 		if (total >= 5)
 		{
 			return true;
@@ -151,23 +151,21 @@ public class checks
 		if (x < 14 && y > 0 && board[x+1][y-1] == player) //to the left of the piece
 		{
 			total++;
-			if (x < 13 && y > 1 && board[x+2][y-2] == player && x > 1) //to the left of the piece
+			if (x < 13 && y > 1 && board[x+2][y-2] == player) //to the left of the piece
 			{
 				total++;
 				if (x < 12 && y > 2 && board[x+3][y-3] == player) //to the left of the piece
 				{
 					total++;
-					if (x < 11 && y > 3 && board[x+4][y-4] == player && x > 3) //to the left of the piece
+					if (x < 11 && y > 3 && board[x+4][y-4] == player) //to the left of the piece
 					{
 						total++;
 					}
 				}
 			}
 		}
-		
-		
-		
-		if (x > 0 && y < 14 && board[x-1][y+1] == player && x < 14) //to the right of the piece
+
+		if (x > 0 && y < 14 && board[x-1][y+1] == player) //to the right of the piece
 		{
 			total++;
 			if (x > 1 && y < 13 && board[x-2][y+2] == player) //to the right of the piece
@@ -184,19 +182,19 @@ public class checks
 			}
 		}
 
-		if (total >= 5) 
+		if (total >= 5)
 		{
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	public boolean checkTie(int[][] board)  //checks if every spot on the board is filled and then determines it's a tie
 	{
-		for (int row = 0; row < board.length; row++) 
+		for (int row = 0; row < board.length; row++)
 		{
-			for (int column = 0; column < board[0].length; column++) 
+			for (int column = 0; column < board[0].length; column++)
 			{
 				if (board[row][column] == BLANK)
 				{
@@ -206,5 +204,5 @@ public class checks
 		}
 		return true;
 	}
-	
+
 }
